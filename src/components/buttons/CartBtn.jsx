bbe80ba6e866d0e7c877d6e35c8c7cea60bb842e
abc <duranakyol71@gmail.com>
2023-07-12 15:35:39 +0200

    created Header,Navbar

---
+++ src/components/buttons/CartBtn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const CartBtn = () => {
+  return (
+    <>
+      <NavLink to="/cart" className="btn btn-outline-primary ms-2">
+        <span className="fa fa-shopping-cart me-2"> Cart (0)</span>
+      </NavLink>
+    </>
+  );
+};
+
+export default CartBtn;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
