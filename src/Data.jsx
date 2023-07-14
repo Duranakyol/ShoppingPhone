a80d2bf25e2a80078784d5660dfdae28d3b12ac3
abc <duranakyol71@gmail.com>
2023-07-14 14:58:47 +0200

    creeate ProductPage

---
+++ src/Data.jsx
@@ -0,0 +1,18 @@
+const DATA = [
+  {
+    id: 0,
+    title: "Iphone 12",
+    price: "750 €",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi magnam exercitationem eos vitae totam. Vitae quam sed, eum consequuntur labore reiciendis, aliquid voluptatum assumenda accusamus incidunt nisi debitis atque alias!",
+    img: "/assets/images/products/iphone12.jpg",
+  },
+  {
+    id: 1,
+    title: "Xiaomi Mi 11",
+    price: "500 €",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi magnam exercitationem eos vitae totam. Vitae quam sed, eum consequuntur labore reiciendis, aliquid voluptatum assumenda accusamus incidunt nisi debitis atque alias!",
+    img: "/assets/images/products/xiaomi11.jpg",
+  },
+];
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import DATA from "../Data";
 
 const Product = () => {
-  return <div>Product</div>;
+  const cardItem = (item) => {
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={item.img} className="card-img-top" alt={item.title} />
+        <div className="card-body">
+          <h5 className="card-title">{item.title}</h5>
+          <p className="lead">{item.price}</p>
+          <a href="/" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1>Product</h1>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">{DATA.map(cardItem)}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
